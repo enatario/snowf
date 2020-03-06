6ff7987aa8158be1bd011f5124dd5e79f14fce9a
Levi <levimcg@gmail.com>
2020-03-06 17:05:29 -0500

    Added filters and transforms

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
+const htmlMinify = require('./src/transforms/htmlMinifiy');
+const dateFormatted = require('./src/filters/dateFormatted');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass/');
   eleventyConfig.addWatchTarget('./src/js/');
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.addPassthroughCopy('./src/js');
 
+  // Transforms
+  eleventyConfig.addTransform('htmlmin', htmlMinify);
+  
+  // Filters
+  eleventyConfig.addFilter('dateFormatted', dateFormatted);
+  
   return {
     dir: {
       input: 'src',
