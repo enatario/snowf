b35c63ff11a03a18f716f9a94411b57db1999153
enatario <elaina.natario@gmail.com>
2020-09-25 15:38:29 -0400

    add js linting

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
 import { terser } from "rollup-plugin-terser";
 import babel from "@rollup/plugin-babel";
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
-  input: './src/js/app.js',
+  input: "./src/js/app.js",
   output: {
-      file: './dist/app.js',
-      format: 'iife',
-      name: 'app'
+    file: "./dist/app.js",
+    format: "iife",
+    name: "app"
   },
   plugins: [
     babel({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**'
+      babelHelpers: "bundled",
+      exclude: "node_modules/**"
     }),
     nodeResolve(),
     terser()
